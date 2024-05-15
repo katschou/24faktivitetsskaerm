761f4223c538c52a18f2cdbddfcd3ca9ab35ce52
katr5 <kas004@edu.zealand.dk>
2024-05-15 12:08:00 +0200

    stretched links on cards

--- index.php
+++ index.php
@@ -25,55 +25,57 @@ require "settings/init.php";
 <div class="container" style="width:1130px; background-color: #E0CEB4">
 
 
-<div class="container">
-    <div class="overskrift text-center">
-        <div>Hvad er du interesseret i?</div>
+    <div class="container">
+        <div class="overskrift text-center">
+            <div>Hvad er du interesseret i?</div>
+        </div>
     </div>
-</div>
 
-<div class="container">
-    <div class="underskrift text-center">
-        <div>Tryk på en af de fire kategorier nedenfor</div>
+    <div class="container">
+        <div class="underskrift text-center">
+            <div>Tryk på en af de fire kategorier nedenfor</div>
+        </div>
     </div>
-</div>
 
-<div class="container">
-    <div class="row">
-        <div class="position-static">
+    <div class="container">
+        <div class="row row-cols-2">
 
-            <div class="card card-index col-6">
-                <img src="img/romantik.png" alt="To mennesker der klinker vinglas">
+
+            <div class="card col card-index p-0" style="width: 500px; height:500px;">
+                <a href="#" class="stretched-link"></a>
+                <img src="img/romantik300.webp" alt="To mennesker der klinker vinglas">
                 <div class="card-img-overlay">
                     <h5 class="card-tekst text-bg-lysebrun">Romantik</h5>
                 </div>
             </div>
 
-            <div class="card card-index col-6">
-                <img src="img/sport.png" alt="En golfbold på en golfplæne">
+            <div class="card col card-index p-0" style="width: 500px; height:500px">
+                <a href="#" class="stretched-link"></a>
+                <img src="img/sport300.webp" alt="En golfbold på en golfplæne">
                 <div class="card-img-overlay">
                     <h5 class="card-tekst text-bg-lysebrun">Sport</h5>
                 </div>
             </div>
 
-            <div class="card card-index col-6">
-                <img src="img/kultur.png" alt="Holbæk gågade">
+            <div class="card col card-index p-0" style="width: 500px; height:500px">
+                <a href="#" class="stretched-link"></a>
+                <img src="img/kultur300.webp" alt="Holbæk gågade">
                 <div class="card-img-overlay">
                     <h5 class="card-tekst text-bg-lysebrun">Kultur</h5>
                 </div>
             </div>
 
-            <div class="card card-index col-6">
-                <img src="img/afslapning.png" alt="En terrasse med udsigt til fjorden">
+            <div class="card col card-index p-0" style="width: 500px; height:500px">
+                <a href="#" class="stretched-link"></a>
+                <img src="img/afslapning300.webp" alt="En terrasse med udsigt til fjorden">
                 <div class="card-img-overlay">
                     <h5 class="card-tekst text-bg-lysebrun">Afslapning</h5>
                 </div>
             </div>
-
         </div>
     </div>
 </div>
 
-</div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
