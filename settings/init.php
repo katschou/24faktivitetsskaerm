34696d457ee5c740161cba45374cdbe73b6ff0cd
katr5 <kas004@edu.zealand.dk>
2024-05-31 12:52:47 +0200

    Færdig :))

--- settings/init.php
+++ settings/init.php
@@ -8,10 +8,19 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_STRICT);
 define("CONFIG_LIVE", "0"); // 0: Test environment || 1: Live environment
 
 if(CONFIG_LIVE == 0){
+    /*
     $DB_SERVER = "localhost";
     $DB_NAME = "aktiviteter";
     $DB_USER = "root";
-    $DB_PASS = "";
+    $DB_PASS = "root";
+*/
+
+    $DB_SERVER = "mysql97.unoeuro.com";
+    $DB_NAME = "katrinaschouby_dk_db_aktiviteter";
+    $DB_USER = "katrinaschouby_dk";
+    $DB_PASS = "z6nEpmR35yHkachFgAw2";
+
+
 }else{
     $DB_SERVER = "";
     $DB_NAME = "";
